a7ac6696ac96478c91001d64be905813bce3bc50
Sameh Mabrouk <smabrouk@mobiquityinc.com>
2016-12-26 17:07:17 +0100

    Initial Commit

---
+++ OpenAir/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OpenAir
+//
+//  Created by Sameh Mabrouk on 26/12/16.
+//  Copyright © 2016 Sameh Mabrouk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
