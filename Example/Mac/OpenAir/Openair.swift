09f6936ddd7b0867ede62b2a68c933d093d5c643
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2018-01-30 17:39:43 +0100

    Adopting Examples for new structure + removing untracked files

--- Example/Mac/OpenAir/Openair.swift
+++ Example/Mac/OpenAir/Openair.swift
@@ -12,13 +12,10 @@ import OpenAirSwift
 class Openair {
     
     // XML API constants
-    private static let namespace     = "default"
     private static let apiKey        = "MSC2kxYUFYkm7rJ8z50z"
     
     let consoleIO = ConsoleIO()
-    let apiManager = OpenairAPIManager(for: namespace, with: apiKey)
-    var calendarComponents: CalendarDateComponents? = nil
-    var endDateCalendarComponents: CalendarDateComponents? = nil
+    let apiManager = OpenairAPIManager(to: RequestConfiguration(key: apiKey))
     var index = 1
     
     func interactiveMode() {
@@ -38,20 +35,21 @@ class Openair {
                 
                 //1- Authenticate user
                 self.consoleIO.writeMessage("👉  Authenticating user...")
-                apiManager.authenticateUser(company: company, userName: userName, password: password, callback: { (success, result) in
+                let login = Login(login: userName, password: password, company: company)
+                apiManager.authenticateUser(login: login, callback: { (success, result) in
                     self.consoleIO.writeMessage("✅  User Authenticated successfully")
                     
                     // Get start date of the current week
-                    let startDateOfWeek = Date().startOfWeek
+                    let startDateOfWeek = Date().startOfWeek()
                     
-                    // Get Year, Month and Day of start date of the current week
-                    self.calendarComponents = startDateOfWeek?.calendarComponents
+                    // Get end date of the current week
+                    let endDateOfWeek = Date().endOfWeek()
                     
                     let userId = result
                     
                     //2- Check if timesheet already exist or not
                     self.consoleIO.writeMessage("👉  Checking if timesheet exists...")
-                    self.apiManager.checkTimesheetExistsForDate(year: (self.calendarComponents?.0)!, month: (self.calendarComponents?.1)!, day: (self.calendarComponents?.2)!, company: company, userName: userName, password: password, callback: { (success, result) in
+                    self.apiManager.checkTimesheetExistsForDate(date: startDateOfWeek, login: login, callback: { (success, result) in
                         if success {
                             self.consoleIO.writeMessage("Hey, Sorry timesheet existed you cannot create one for this week")
                         }
@@ -61,14 +59,8 @@ class Openair {
                             //self.consoleIO.writeMessage("Creating new timesheet...")
                             self.consoleIO.writeMessage("👉  Listing projects...")
                             
-                            // Get end date of the current week
-                            let endDateOfWeek = Date().endOfWeek
-                            
-                            // Get Year, Month and Day of end date of the current week
-                            self.endDateCalendarComponents = endDateOfWeek?.calendarComponents
-                            
                             //3- Get list of project names
-                            self.apiManager.getProjects(company: company, userName: userName, password: password, callback: { (success, result) in
+                            self.apiManager.getProjects(login: login,  callback: { (success, result) in
                                 
                                 if success {
                                     self.consoleIO.writeMessage("✅  Done")
@@ -89,15 +81,15 @@ class Openair {
                                                 
                                                 //4- get project task
                                                 self.consoleIO.writeMessage("👉  Getting project tasks...")
-                                                self.apiManager.getprojectTasks(projectId: projects[projectId].id, company: company, userName: userName, password: password, callback: { (success, result) in
+                                                self.apiManager.getprojectTasks(projectId: projects[projectId].id, login: login, callback: { (success, result) in
                                                     
                                                     if success {
                                                         self.consoleIO.writeMessage("✅  Done")
                                                         // print each project task to console
-                                                        let projectTasks = result as? [ProjectTask]
-                                                        if let projectTasks = projectTasks {
+
+                                                        if let projectTasks = result as? [ProjectTask] {
                                                             for (index, projectTask) in projectTasks.enumerated() {
-                                                                self.consoleIO.writeMessage(String(index) + "." + projectTask.name, to: .projectTasks)
+                                                                self.consoleIO.writeMessage(String(index) + "." + (projectTask.name ?? "missingName"), to: .projectTasks)
                                                             }
                                                             
                                                             self.consoleIO.writeMessage("👉  Now select project task...")
@@ -116,32 +108,17 @@ class Openair {
                                                                     self.consoleIO.writeMessage("🚀  creating timesheet...")
                                                                     
                                                                     //5- Add timesheet for current week
-                                                                    self.apiManager.addTimesheet(startDateComponents: self.calendarComponents!, endDateComponents: self.endDateCalendarComponents!, userId: userId as! String, company: company, userName: userName, password: password, callback: { (success, result) in
+                                                                    self.apiManager.addTimesheet(startDate: startDateOfWeek, endDate: endDateOfWeek, userId: userId as! String, login: login, callback: { (success, result) in
                                                                         if success {
                                                                             self.consoleIO.writeMessage("✅  Timesheet created successfully")
-                                                                            
-                                                                            if let calendarComponents = self.calendarComponents {
                                                                                 
-                                                                                var currentDateComponents = DateComponents()
-                                                                                currentDateComponents.year = calendarComponents.0
-                                                                                currentDateComponents.month = calendarComponents.1
-                                                                                currentDateComponents.day = calendarComponents.2
-                                                                                
-                                                                                let date =  Date.dateFromComponents(dateComponents: currentDateComponents)
-                                                                                let newDate = date?.dateComponentsByAddingDays(days: self.index)
-                                                                                let newcalendarComponents = newDate?.calendarComponents
-                                                                                
-                                                                                if let newcalendarComponents = newcalendarComponents {
+                                                                                let date = startDateOfWeek.add(days: self.index)
                                                                                     
                                                                                     //6- Add timesheet tasks, e.g. work hour per day
-                                                                                    self.addTaskToTimesheet(dateComponent: newcalendarComponents, weekDaysWorkHours: weekDaysWorkHours, timesheetId: result as! String, projectId: projects[projectId].id , projectTaskId: projectTasks[taskId].id, userId: userId as! String, company: company, userName: userName, password: password)
+                                                                                    self.addTaskToTimesheet(date: date, weekDaysWorkHours: weekDaysWorkHours, timesheetId: result as! String, projectId: projects[projectId].id , projectTaskId: projectTasks[taskId].id ?? "none", userId: userId as! String, login: login)
                                                                                 }
-                                                                            }
                                                                         }
-                                                                        else {
-                                                                            self.consoleIO.writeMessage("An error was encountered , Timesheet is not created 🙁")
-                                                                        }
-                                                                    })
+                                                                    )
                                                                 }
                                                             }
                                                         }
@@ -182,7 +159,7 @@ class Openair {
         var weekDaysWorkHours = [Int]()
         
         for index in 0...4 {
-            self.consoleIO.writeMessage(DayOfWeek.fromNumber(number: index).description + ": ", to: .timesheetTasks)
+            self.consoleIO.writeMessage(DayOfWeek.at(number: index).description + ": ", to: .timesheetTasks)
             let workHour = Int(self.consoleIO.getInput())
             if workHour != nil {
                 if let workHour = workHour {
@@ -196,10 +173,10 @@ class Openair {
         return weekDaysWorkHours
     }
     
-    func addTaskToTimesheet(dateComponent: CalendarDateComponents, weekDaysWorkHours: [Int], timesheetId: String, projectId: String, projectTaskId: String, userId: String, company: String, userName: String, password: String) {
+    func addTaskToTimesheet(date: Date, weekDaysWorkHours: [Int], timesheetId: String, projectId: String, projectTaskId: String, userId: String, login: Login) {
         
         // add tasks to timesheet for 5 working days.
-        apiManager.addTaskToTimesheet(dateComponents: dateComponent, hours: String(weekDaysWorkHours[index-1]), timesheetId: timesheetId, projectId: projectId, projectTaskId: projectTaskId, userId: userId, company: company, userName: userName, password: password) { (success, result) in
+        apiManager.addTaskToTimesheet(date: date, hours: weekDaysWorkHours[index-1], timesheetId: timesheetId, projectId: projectId, projectTaskId: projectTaskId, userId: userId, login: login) { (success, result) in
             if success {
                 
                 self.index = self.index + 1
@@ -208,7 +185,7 @@ class Openair {
                     
                     // save and submit timesheet
                     print("Added 5 days tasks")
-                    self.apiManager.submitTimesheet(company: company, userName: userName, password: password, timesheetId: timesheetId, callback: { (success, result) in
+                    self.apiManager.submitTimesheet(login: login, timesheetId: timesheetId, callback: { (success, result) in
                         if success {
                             print("All done! 🎉 timesheet submitted, Enjoy your weekend! 🚀")
                         }
@@ -221,22 +198,7 @@ class Openair {
                 
                 self.consoleIO.writeMessage("Task added to timesheet...")
                 
-                if let calendarComponents = self.calendarComponents {
-                    var currentDateComponents = DateComponents()
-                    currentDateComponents.year = calendarComponents.0
-                    currentDateComponents.month = calendarComponents.1
-                    currentDateComponents.day = calendarComponents.2
-                    
-                    let date =  Date.dateFromComponents(dateComponents: currentDateComponents)
-                    
-                    let date2 = date?.dateComponentsByAddingDays(days: self.index)
-                    let newcalendarComponents = date2?.calendarComponents
-                    
-                    if let newcalendarComponents = newcalendarComponents {
-                        
-                        self.addTaskToTimesheet(dateComponent: newcalendarComponents, weekDaysWorkHours: weekDaysWorkHours, timesheetId: timesheetId, projectId: projectId, projectTaskId: projectTaskId, userId: userId, company: company, userName: userName, password: password)
-                    }
-                }
+                self.addTaskToTimesheet(date: Date().add(days: self.index), weekDaysWorkHours: weekDaysWorkHours, timesheetId: timesheetId, projectId: projectId, projectTaskId: projectTaskId, userId: userId, login: login)
             }
         }
     }
