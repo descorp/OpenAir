20b0da3e627c074c2d572be156e4821bf3e005d3
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2018-01-31 16:30:53 +0100

    Renaming "Example" folder into "Examples" and removing pods dependencies from Mac project

--- Examples/OpenAirTool/Openair.swift
+++ Examples/OpenAirTool/Openair.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import OpenAirSwift
 
 class Openair {
     
---
+++ Tests/OpenAirTool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OpenAirTool
+//
+//  Created by Vladimir Abramichev on 31/01/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
